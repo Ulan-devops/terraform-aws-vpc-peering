59478184e6982c6613e3fe887258fa3ee650944a
Huseyin Akten <husakten@gmail.com>
2020-01-30 12:53:28 -0600

    sadfa

--- module-vpc-1.tf
+++ module-vpc-1.tf
@@ -2,18 +2,18 @@ module "vpc-1" {
   source = "./module-vpc-1"
   
 }
-# output "vpc_id" {
-#   value = "${module.vpc.vpc_id}"
-# }
+output "vpc_id_1" {
+  value = "${module.vpc.vpc_id}"
+}
 
-# output "vpc_cidr" {
-#   value = "${module.vpc.vpc_cidr}"
-# }
+output "vpc_cidr_1" {
+  value = "${module.vpc.vpc_cidr}"
+}
 
-# output "public_subnets" {
-#   value = "${module.vpc.public_subnets}"
-# }
+output "public_subnets_1" {
+  value = "${module.vpc.public_subnets}"
+}
 
-# output "private_subnets" {
-#   value = "${module.vpc.private_subnets}"
-# }
+output "private_subnets_1" {
+  value = "${module.vpc.private_subnets}"
+}
--- module-vpc-2.tf
+++ module-vpc-2.tf
@@ -2,18 +2,18 @@ module "vpc-2" {
   source = "./module-vpc-2"
   
 }
-# output "vpc_id" {
-#   value = "${module.vpc.vpc_id}"
-# }
+output "vpc_id_2" {
+  value = "${module.vpc.vpc_id}"
+}
 
-# output "vpc_cidr" {
-#   value = "${module.vpc.vpc_cidr}"
-# }
+output "vpc_cidr_2" {
+  value = "${module.vpc.vpc_cidr}"
+}
 
-# output "public_subnets" {
-#   value = "${module.vpc.public_subnets}"
-# }
+output "public_subnets_2" {
+  value = "${module.vpc.public_subnets}"
+}
 
-# output "private_subnets" {
-#   value = "${module.vpc.private_subnets}"
-# }
+output "private_subnets_2" {
+  value = "${module.vpc.private_subnets}"
+}

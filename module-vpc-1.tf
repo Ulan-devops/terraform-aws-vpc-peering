ff6eba4f046b42bdc8e85d8656fb9abd7894efbb
Huseyin Akten <husakten@gmail.com>
2020-02-03 00:39:51 -0600

    sdfa

--- module-vpc-1.tf
+++ module-vpc-1.tf
@@ -1,19 +1,34 @@
-module "vpc-1" {
-  source = "./module-vpc-1"
-  
-}
-output "vpc_id_1" {
-  value = "${module.vpc-1.vpc-id}"
+
+
+
+module "vpc" {
+  source = "git::https://github.com/hakten/module-vpc.git"
+  provider "aws" {
+  region = "us-east-1"
 }
 
-output "vpc_cidr_1" {
-  value = "${module.vpc-1.vpc_cidr}"
+cidr            = "10.0.0.0/16"
+
+project         = "Wordpress"
+environment     = "Test"
+
+azs             = ["us-east-1a","us-east-1b","us-east-1c"]
+public_subnets  = ["10.0.1.0/24","10.0.2.0/24","10.0.3.0/24"]
+private_subnets = []
 }
 
-output "public_subnets_1" {
-  value = "${module.vpc-1.public_subnets}"
+output "vpc-id-1" {
+  value = "${module.vpc.vpc-id}"
 }
 
-output "private_subnets_1" {
-  value = "${module.vpc-1.private_subnets}"
+output "vpc-cidr-1" {
+  value = "${module.vpc.vpc-cidr}"
+}
+
+output "public-subnets-1" {
+  value = "${module.vpc.public-subnets}"
+}
+
+output "private-subnets-1" {
+  value = "${module.vpc.private-subnets}"
 }
--- module-vpc-2.tf
+++ module-vpc-2.tf
@@ -1,19 +1,31 @@
-module "vpc-2" {
-  source = "./module-vpc-2"
-  
-}
-output "vpc_id_2" {
-  value = "${module.vpc-2.vpc_id}"
+module "vpc" {
+  source = "git::https://github.com/hakten/module-vpc.git"
+  provider "aws" {
+  region = "us-east-2"
 }
 
-output "vpc_cidr_2" {
-  value = "${module.vpc-2.vpc_cidr}"
+cidr            = "192.168.0.0/16"
+
+project         = "Wordpress"
+environment     = "Test"
+
+azs             = ["us-east-2a","us-east-2b","us-east-2c"]
+public_subnets  = ["192.168.1.0/24","192.168.2.0/24","192.168.3.0/24"]
+private_subnets = []
 }
 
-output "public_subnets_2" {
-  value = "${module.vpc-2.public_subnets}"
+output "vpc-id-2" {
+  value = "${module.vpc.vpc-id}"
 }
 
-output "private_subnets_2" {
-  value = "${module.vpc-2.private_subnets}"
+output "vpc-cidr-2" {
+  value = "${module.vpc.vpc-cidr}"
+}
+
+output "public-subnets-2" {
+  value = "${module.vpc.public-subnets}"
+}
+
+output "private-subnets-2" {
+  value = "${module.vpc.private-subnets}"
 }
